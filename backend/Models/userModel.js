f5063bc2ebeb459ae2da6b974237cd475f60aace
Rishabh Jain <rishabhjain7458@gmail.com>
2024-12-10 00:15:35 +0530

    Bug fixes

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -2,56 +2,52 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-
-    name:{
-        type : String,
-        required:[true, 'Please enter your name.']
+    name: {
+        type: String,
+        required: [true, 'Please enter your name.']
     },
-    email:{
-        type:String,
-        required:[true,'Please enter an email.'],
+    email: {
+        type: String,
+        required: [true, 'Please enter an email.'],
         unique: true,
-        lowercase:true,
-        validate: [validator.isEmail,"Please enter a valid email."]
+        lowercase: true,
+        validate: [validator.isEmail, "Please enter a valid email."]
     },
-
-    password:{
-       type:String,
-       required:[true,'Please enter a Password'],
-       minlength:8 
+    password: {
+        type: String,
+        required: [true, 'Please enter a Password'],
+        minlength: 8
     },
-    confirmPassword:{
-        type:String,
-        required:[true,'Please confirm your Password'],
-        validate:{
-            validator : function(val){
-                return val== this.password;
+    confirmPassword: {
+        type: String,
+        required: [true, 'Please confirm your Password'],
+        validate: {
+            validator: function(val) {
+                return val == this.password;
             },
-            message:"Password $ Confirm Password does not match!"
+            message: "Password & Confirm Password do not match!"
         }
-     }
-    
-})
+    },
+    designation: {
+        type: String,
+        required: true, 
+    },
+});
 
 const bcrypt = require('bcrypt');
-userSchema.pre('save',async function(next){
-    if(!this.isModified('password')) return next();
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
 
-    this.password = await bcrypt.hash(this.password,12);
+    this.password = await bcrypt.hash(this.password, 12);
     this.confirmPassword = undefined;
     next();
-})
+});
 
-
-//making a method to verify DB passaword and user given Password
-//bcrypt.compare()
-
-userSchema.methods.comparePasswordInDb=async function(pass,pwsDB){
-    return bcrypt.compare(pass,pwsDB)
+// Making a method to verify DB password and user given password
+userSchema.methods.comparePasswordInDb = async function(pass, pwsDB) {
+    return bcrypt.compare(pass, pwsDB);
 }
 
-
-
-//creating model
-const User = mongoose.model('User',userSchema);
-module.exports = User ;
+// Creating model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
